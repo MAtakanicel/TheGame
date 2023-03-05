ca843a94b83be1e22130b076eeafd0de0642af35
Halit Akca <halitakca070@gmail.com>
2023-03-05 20:13:39 +0300

    de

--- Warrior.dart
+++ Warrior.dart
@@ -3,4 +3,7 @@ import 'Player.dart';
 class Warrior extends Player{
 @override
   int get baseHealth => 100;
+  @override
+  // TODO: implement baseCriticalChance
+  int get baseCriticalChance => 10;
 }
